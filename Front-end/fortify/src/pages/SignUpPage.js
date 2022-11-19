9f1fc0e33f9c086e35b0614cf5cfcd3f3c64645f
gnawd <zqwa@deakin.edu.au>
2022-11-19 14:01:13 +1100

    Wrote a template

---
+++ Front-end/fortify/src/pages/SignUpPage.js
@@ -0,0 +1,4 @@
+const SignUpPage = () => {
+    return <h1>this is the Sign-Up Page</h1>
+  }
+  export default SignUpPage
---
+++ Front-end/fortify/src/pages/SigninPage.js
@@ -0,0 +1,4 @@
+const SignInPage = () => {
+    return <h1>this is the Sign-In Page</h1>
+  }
+  export default SignInPage
