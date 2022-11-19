b7eba46ccd293dd7540d524cdadfe2da45002cae
gnawd <zqwa@deakin.edu.au>
2022-11-19 17:10:31 +1100

    Created the sign up page and played around with CSS/HTML

--- Front-end/fortify/src/pages/SignUpPage.js
+++ Front-end/fortify/src/pages/SignUpPage.js
@@ -1,4 +1,35 @@
 const SignUpPage = () => {
-    return <h1>this is the Sign-Up Page</h1>
+    return <div class="background">
+    <form action="#">
+
+      <div class="wrap">
+    <h2>Join our company today!</h2>
+    <form action="#">
+      <div class="input-box">
+        <input type="text" placeholder="Enter your name" required />
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Enter your email" required />
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Create password" required />
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Confirm password" required />
+      </div>
+      <div class="policy">
+        <input type="checkbox" />
+        <h3>I accept all terms & condition</h3>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Register Now" />
+      </div>
+      <div class="text">
+        <h3>Already have an account? <a href="#">Login here</a></h3>
+      </div>
+    </form>
+      </div>
+    </form>
+  </div>
   }
   export default SignUpPage
